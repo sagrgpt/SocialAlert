020968b5279a5660289bf26554ede297b9d0825a
Sagar Gupta <sgrkmr12@gmail.com>
2017-05-09 14:02:17 +0530

    Alert and api 23 support added!

--- app/src/main/java/com/example/socialalert/AlertActivity.java
+++ app/src/main/java/com/example/socialalert/AlertActivity.java
@@ -1,5 +1,7 @@
 package com.example.socialalert;
 
+import android.*;
+import android.Manifest;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -9,7 +11,11 @@ import android.graphics.Color;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -20,11 +26,16 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.messaging.FirebaseMessaging;
+
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -35,7 +46,7 @@ import es.dmoral.toasty.Toasty;
 import static java.text.DateFormat.getDateInstance;
 import static java.text.DateFormat.getTimeInstance;
 
-public class AlertActivity extends AppCompatActivity {
+public class AlertActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
 
     TextView settings;
@@ -46,19 +57,36 @@ public class AlertActivity extends AppCompatActivity {
 
     LocationManager manager;
     LocationListener locationListener;
+    GoogleApiClient mGoogleApiClient;
 
     DatabaseReference databaseReference;
     String key;
     AlertData changeData;
+    private static final int LOCATION_ACCESS_PERMISSION = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alert);
 
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if(mGoogleApiClient == null){
+                mGoogleApiClient = new GoogleApiClient.Builder(this)
+                        .addConnectionCallbacks(this)
+                        .addOnConnectionFailedListener(this)
+                        .addApi(LocationServices.API)
+                        .build();
+            }
+        }
+
         settings = (TextView) findViewById(R.id.settings);
         alert_btn = (Button) findViewById(R.id.alert_btn);
         alertLayout = (ViewGroup) findViewById(R.id.alertLayout);
+
+        //Subscribe to firebase notification topic 'default topic for all'
+        FirebaseMessaging.getInstance().subscribeToTopic("All");
+        Toast.makeText(getApplicationContext(), "TOpic Suscribed", Toast.LENGTH_SHORT).show();
+        Log.d("Firebase Topic", "Subscribed to news topic");
 
 
         //Call settings on preference not set
@@ -80,22 +108,46 @@ public class AlertActivity extends AppCompatActivity {
     }
 
     public void onNetworkClicked(View view) {
-        startActivity(new Intent(AlertActivity.this,NetworkActivity.class));
+        startActivity(new Intent(AlertActivity.this, NetworkActivity.class));
         finish();
     }
 
     public void onAlert(View view) {
+        //Check if the preferences for contact has already been set..
         if (!sharedPreferences.getBoolean("IsSet", false)) {
             return;
         }
-        if(!alertACTIVE) {
-            Toasty.info(getApplicationContext(),"Getting location...",Toast.LENGTH_SHORT).show();
+        if (!alertACTIVE) {
+            Toasty.info(getApplicationContext(), "Getting location...", Toast.LENGTH_SHORT).show();
             alertACTIVE = true;
             alert_btn.setText(R.string.btn_text_cancel);
-            getLocation();
-        }
-        else{
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+            {
+
+                mGoogleApiClient.connect();
+            }
+            else{
+                getLocation();
+            }
+
+        } else {
             copyData();
+        }
+    }
+
+    public void onBeginLocationUpdate(){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Log.d("location", "Getting user permission for fine location");
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    LOCATION_ACCESS_PERMISSION);
+        }
+        Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if(mLastLocation!=null){
+            String lat = String.valueOf(mLastLocation.getLatitude());
+            String lng = String.valueOf(mLastLocation.getLongitude());
+            Toasty.custom(getApplicationContext(), "Location Received!", R.drawable.ic_location, Color.TRANSPARENT, Toast.LENGTH_SHORT, true, true).show();
+            createNews(lat, lng);
         }
     }
 
@@ -104,48 +156,42 @@ public class AlertActivity extends AppCompatActivity {
         if (!manager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
             buildAlertMessageNoGps();
         }
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
         locationListener = new LocationListener() {
             @Override
             public void onLocationChanged(Location loc) {
-                Log.d("Location",String.valueOf(loc.getLongitude()));
-                Log.d("Location",String.valueOf(loc.getLatitude()));
+                Log.d("Location", String.valueOf(loc.getLongitude()));
+                Log.d("Location", String.valueOf(loc.getLatitude()));
                 String lat = String.valueOf(loc.getLatitude());
                 String lng = String.valueOf(loc.getLongitude());
-                Toasty.custom(getApplicationContext(),"Location Received!",R.drawable.ic_location, Color.TRANSPARENT,Toast.LENGTH_SHORT,true,true).show();
-                createNews(lat,lng);
+                Toasty.custom(getApplicationContext(), "Location Received!", R.drawable.ic_location, Color.TRANSPARENT, Toast.LENGTH_SHORT, true, true).show();
+                createNews(lat, lng);
             }
 
             @Override
             public void onStatusChanged(String provider, int status, Bundle extras) {
+                Log.d("LOCATION","Status Changed called");
 
             }
 
             @Override
             public void onProviderEnabled(String provider) {
-
+                Log.d("LOCATION","onProviderEnabled called");
             }
 
             @Override
             public void onProviderDisabled(String provider) {
+                Log.d("Location","onProviderDisabled called");
 
             }
         };
+        Log.d("LOCATION",String.valueOf(manager.isProviderEnabled(LocationManager.GPS_PROVIDER)));
+        //if current api level is >=23 this function is not called!!
         manager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
 
     }
 
     private void createNews(String lat, String lng) {
-        manager.removeUpdates(locationListener);
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.M) manager.removeUpdates(locationListener);
 
         Calendar c = Calendar.getInstance();
         SimpleDateFormat sdf = (SimpleDateFormat) getTimeInstance();
@@ -215,15 +261,59 @@ public class AlertActivity extends AppCompatActivity {
         databaseReference.removeValue();
         alertACTIVE = false;
         Toasty.info(getApplicationContext(),"Alert removed. Hope you are Safe!",Toast.LENGTH_SHORT).show();
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+            mGoogleApiClient.disconnect();
         alert_btn.setText(R.string.btn_text_alert);
     }
 
     @Override
-    protected void onStop() {
-        super.onStop();
-//        if(alertACTIVE) {
-//            copyData();
-//        }
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        switch(requestCode){
+            case LOCATION_ACCESS_PERMISSION: {
+
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    onBeginLocationUpdate();
+                }
+                else Toasty.error(getApplicationContext(),"Location Permission not granted",
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
+
     }
 
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                Log.d("location", "Getting user permission for fine location");
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        LOCATION_ACCESS_PERMISSION);
+            }
+            onBeginLocationUpdate();
+
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Toasty.error(getApplicationContext(),"Unable to fetch location").show();
+    }
+
+    @Override
+    protected void onStop() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+            mGoogleApiClient.disconnect();
+        if(alertACTIVE) {
+            copyData();
+        }
+        super.onStop();
+    }
 }
